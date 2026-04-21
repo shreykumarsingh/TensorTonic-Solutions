c4fbcf9ab37acbb0633cd4b7e2702510af7fcda9
shreykumarsingh <workmailshreykumarsingh@gmail.com>
2026-04-21 15:02:21 +0530

    Add solution for entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def entropy_node(y):
+    y = np.asarray(y)
+
+    if y.size == 0:
+        return 0.0
+
+    _, counts = np.unique(y, return_counts=True)
+
+    probs = counts / counts.sum()
+
+    return -np.sum(probs * np.log2(probs))
